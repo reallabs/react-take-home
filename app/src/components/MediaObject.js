599058f41da5c54ef22f7bda6aafd06838149d51
Bhekisile <bhekisilejozi@gmail.com>
2024-01-21 21:55:14 +0200

    Add download and copy a link to clipboard buttons

--- app/src/components/MediaObject.js
+++ app/src/components/MediaObject.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { fetchCampaign } from "../redux/campaigns/campaignsSlice";
 import { useDispatch, useSelector } from "react-redux";
 import './styles/MediaObject.css';
+import { FaLink, FaDownload, FaPlay } from "react-icons/fa6";
 
 function MediaObject() {
   const dispatch = useDispatch();
@@ -10,6 +11,12 @@ function MediaObject() {
   useEffect(() => {
     dispatch(fetchCampaign());
   },[dispatch]);
+
+  // const handleMediaType = () => {
+  //   let mediaType = campaignHeader.campaigns.medias.media_type;
+  //   (mediaType === "video") ? <FaPlay /> : " ";
+
+  //   }
 
   return (
     <div className="photo-container">
@@ -30,18 +37,38 @@ function MediaObject() {
             <div className="scrollable-container">
               <ul className="photo">
                 {campaign.medias.map((media) => (
-                  <li><img src={media.cover_photo_url} alt="#"/></li>
-              ))}
+                  <li key={campaign.id} className="media-item">
+                    <img src={media.cover_photo_url} alt="#" />
+                    <div className="button-container">
+                      <button type="button" className="link">
+                        <a
+                          href={media.tracking_link}
+                          className="tracking-link"
+                          target="blank"
+                          rel="noreferrer"
+                        >
+                          <FaLink className="link-size" />
+                        </a>
+                      </button>
+                      <button type="button" className="link">
+                        <a
+                          href={media.download_url}
+                          className="download"
+                          download
+                        >
+                          <FaDownload className="link-size" />
+                        </a>
+                      </button>
+                    </div>
+                    <div className="media-type">
+                      {media.media_type === "video" ? <FaPlay className="play" /> : " "};
+                    </div>
+                  </li>
+                ))}
               </ul>
-            </div>
-              <div>
-                <ul>
-                  <li>{campaign.tracking_link}</li>
-                  <li><img src={campaign.download_url} alt="Download" /></li>
-                </ul>
-              </div>
-            </>
-        ))},
+            </div>             
+          </>
+        ))}
       </div>
     </div>
   );
