9d306ac9124219e2f06c1ed063d120c4b182896a
Bhekisile <bhekisilejozi@gmail.com>
2024-01-21 23:36:43 +0200

    Add navigation bar

--- app/src/components/Navigation.js
+++ app/src/components/Navigation.js
@@ -1,12 +1,13 @@
 import React from "react";
 import * as FaIcons from "react-icons/fa6";
+import './styles/Navigation.css';
 
 function Navigation() {
   return (
     <div className="navbar">
       <FaIcons.FaBars />
-      Campaigns
-      129.5k
+      <div className="campaign">CAMPAIGNS</div>
+      <div className="total-visit">129.5k</div>
     </div>
   );
 }
